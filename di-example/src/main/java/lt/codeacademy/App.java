8df19dd7be89b7254728259d83230ea5d4827427
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-02 21:41:43 +0200

    1. [DI example] DI example with Spring Context and Configuration class using @Bean

--- di-example/src/main/java/lt/codeacademy/App.java
+++ di-example/src/main/java/lt/codeacademy/App.java
@@ -1,8 +1,23 @@
 package lt.codeacademy;
 
+import lt.codeacademy.config.SpringContextConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
+//		diExampleUsingSimpleJava();
+		diExampleUsingSpringContext();
+	}
+
+	private static void diExampleUsingSpringContext() {
+		ApplicationContext springContext = new AnnotationConfigApplicationContext(SpringContextConfig.class);
+		GradeService gradeService = springContext.getBean(GradeService.class);
+		System.out.println("Vidurkis: " + gradeService.avarageGrade());
+	}
+
+	private static void diExampleUsingSimpleJava() {
 		final GradeService gradeService = new GradeService(new ExternalMarksDao());
 		System.out.println("Pazymiu vidurkis: " + gradeService.avarageGrade());
 	}
