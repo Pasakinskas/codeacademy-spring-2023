cf7b40f3600081e7ef587ad314d5943bbc8f66a4
Pasakinskas <marius.pasakinskas@gmail.com>
2024-01-03 19:53:38 +0200

    2. [Spring MVC] add initial project with first controller

---
+++ eshop/src/main/java/lt/codeacademy/eshop/FirstController.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.eshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class FirstController {
+
+  @GetMapping("/hello")
+  public String sayHelloToCustomer() {
+    return "hello.html";
+  }
+}
