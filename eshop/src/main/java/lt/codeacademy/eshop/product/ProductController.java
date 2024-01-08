e4109670d489279394013c52aa512ec5ec3809ec
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-08 18:41:10 +0200

    3. [Spring MVC] rename FirstController and move to product package

---
+++ eshop/src/main/java/lt/codeacademy/eshop/product/ProductController.java
@@ -0,0 +1,32 @@
+package lt.codeacademy.eshop.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductController {
+
+	private ProductService productService;
+
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping("/products/create")
+	public String sayHelloToCustomer(Model model) {
+		model.addAttribute("product", new Product());
+		return "product";
+	}
+
+	@PostMapping("/products/create")
+	public String createAProduct(Product product) {
+
+		productService.saveProduct(product);
+		System.out.println("currently in the database");
+		productService.getAllProducts().forEach(System.out::println);
+		return "hello";
+	}
+}
