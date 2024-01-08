396ac69102b6bc8cd0c646b2be1e15563c2bb124
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-08 19:31:11 +0200

    3. [Spring MVC] refactor to have separate http endpoints file for url path as store/routes list

---
+++ eshop/src/main/java/lt/codeacademy/eshop/HttpEndpoints.java
@@ -0,0 +1,8 @@
+package lt.codeacademy.eshop;
+
+public final class HttpEndpoints {
+
+  public static final String PRODUCTS = "/products";
+  public static final String PRODUCTS_CREATE = PRODUCTS + "/create";
+
+}
--- eshop/src/main/java/lt/codeacademy/eshop/product/ProductController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/product/ProductController.java
@@ -1,5 +1,6 @@
 package lt.codeacademy.eshop.product;
 
+import lt.codeacademy.eshop.HttpEndpoints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +16,18 @@ public class ProductController {
 		this.productService = productService;
 	}
 
-	@GetMapping("/products/create")
+	@GetMapping(HttpEndpoints.PRODUCTS_CREATE)
 	public String sayHelloToCustomer(Model model) {
 		model.addAttribute("product", new Product());
 		return "product";
 	}
 
-	@PostMapping("/products/create")
+	@PostMapping(HttpEndpoints.PRODUCTS_CREATE)
 	public String createAProduct(Product product) {
 
 		productService.saveProduct(product);
 		System.out.println("currently in the database");
 		productService.getAllProducts().forEach(System.out::println);
-		return "welcome/welcome";
+		return "welcome/welcome";  // tai nera URL path
 	}
 }
