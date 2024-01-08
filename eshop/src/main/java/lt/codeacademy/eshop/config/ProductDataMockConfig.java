664f06057904f0729299caee17e18774dde2ddd1
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-08 20:35:42 +0200

    3. [Spring MVC] use lombok log4j2 with configuration options

--- eshop/src/main/java/lt/codeacademy/eshop/config/ProductDataMockConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/config/ProductDataMockConfig.java
@@ -2,6 +2,7 @@ package lt.codeacademy.eshop.config;
 
 import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lt.codeacademy.eshop.product.Product;
 import lt.codeacademy.eshop.product.ProductService;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@Log4j2
 public class ProductDataMockConfig {
 
   private static final int MAX_COUNT = 10;
@@ -17,6 +19,7 @@ public class ProductDataMockConfig {
 
   @Bean
   public Void initProducts() {
+    log.atDebug().log("-==== initProducts initialization start ====-");
     var count = 0;
     final Faker faker = new Faker();
     while (MAX_COUNT >= count) {
@@ -28,6 +31,7 @@ public class ProductDataMockConfig {
           .build());
       count++;
     }
+    log.atDebug().log("-==== initProducts initialization end ====-");
     return null;
   }
 }
--- eshop/src/main/java/lt/codeacademy/eshop/product/ProductController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/product/ProductController.java
@@ -2,6 +2,7 @@ package lt.codeacademy.eshop.product;
 
 import java.util.List;
 
+import lombok.extern.log4j.Log4j2;
 import lt.codeacademy.eshop.HttpEndpoints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Log4j2
 public class ProductController {
 
 	private ProductService productService;
@@ -20,6 +22,7 @@ public class ProductController {
 
 	@GetMapping(HttpEndpoints.PRODUCTS_CREATE)
 	public String sayHelloToCustomer(Model model) {
+    log.atInfo().log("-==== get product on create ====-");
 		model.addAttribute("product", Product.builder().build());
 		return "product/product";
 	}
