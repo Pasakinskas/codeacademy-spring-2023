3715c59f55d8087b1ec771b5bbe7232c547430cf
Pasakinskas <marius.pasakinskas@gmail.com>
2024-01-10 20:47:15 +0200

    5. [DATA JPA] use PageableDefault annotation and add pagination bar stub

--- eshop/src/main/java/lt/codeacademy/eshop/product/controllers/ProductController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/product/controllers/ProductController.java
@@ -10,6 +10,7 @@ import lt.codeacademy.eshop.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -57,7 +58,7 @@ public class ProductController {
   }
 
   @GetMapping(HttpEndpoints.PRODUCTS)
-  public String getProducts(Model model, Pageable pageable) {
+  public String getProducts(Model model, @PageableDefault(size = 3) Pageable pageable) {
     final Page<ProductDto> allProducts = productService.getAllProductsPage(pageable);
     model.addAttribute("productList", allProducts);
 
