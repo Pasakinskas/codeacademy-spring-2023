33ce2dba517c233775e3c533386e616a54a20aee
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-15 18:51:04 +0200

    6. [Spring MVC, DATA JPA] add sorting icons and links (pagination)

--- eshop/src/main/java/lt/codeacademy/eshop/product/controllers/ProductController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/product/controllers/ProductController.java
@@ -10,6 +10,7 @@ import lt.codeacademy.eshop.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -58,7 +59,8 @@ public class ProductController {
   }
 
   @GetMapping(HttpEndpoints.PRODUCTS)
-  public String getProducts(Model model, @PageableDefault(size = 5) Pageable pageable) {
+  public String getProducts(Model model,
+                            @PageableDefault(size = 5, sort = {"price"}, direction = Sort.Direction.ASC) Pageable pageable) {
     final Page<ProductDto> allProducts = productService.getAllProductsPage(pageable);
     model.addAttribute("productList", allProducts);
 
