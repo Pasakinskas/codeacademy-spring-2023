f05a66a3e89ab508d603f87c4d05080b10fb5425
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-22 21:35:28 +0200

    9. [Custom Error Pages] add global ModelAttribute

--- eshop/src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
+++ eshop/src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -1,6 +1,7 @@
 package lt.codeacademy.eshop.common;
 
 import java.beans.PropertyEditor;
+import java.util.Date;
 
 import lt.codeacademy.eshop.product.exception.ProductNotFoundException;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
@@ -23,6 +25,11 @@ public class ApplicationControllerAdvice {
   public void initBinder(WebDataBinder webDataBinder) {
     StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
     webDataBinder.registerCustomEditor(String.class, trimmerEditor);
+  }
+
+  @ModelAttribute("modelDateAttr")  // modelDate is optional here, HTML will use now (as method name)
+  public Date now() {
+    return new Date();
   }
 
 }
