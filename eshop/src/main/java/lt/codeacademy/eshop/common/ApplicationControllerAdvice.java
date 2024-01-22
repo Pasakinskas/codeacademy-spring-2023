2cf62ec1d70947b4206d21e15bd7bdec61aab09a
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-22 21:30:18 +0200

    9. [Custom Error Pages] InitBinder example to trim spaces for string text

--- eshop/src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
+++ eshop/src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -1,9 +1,14 @@
 package lt.codeacademy.eshop.common;
 
+import java.beans.PropertyEditor;
+
 import lt.codeacademy.eshop.product.exception.ProductNotFoundException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
@@ -14,5 +19,10 @@ public class ApplicationControllerAdvice {
     return "product/error/productNotFound";
   }
 
+  @InitBinder
+  public void initBinder(WebDataBinder webDataBinder) {
+    StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+    webDataBinder.registerCustomEditor(String.class, trimmerEditor);
+  }
 
 }
