bc80438044005ab8b9ddcf4311fcc3e48e56a770
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-23 21:42:25 +0200

    10. [Spring MVC, Session] create session and save also retrieve value from session using Servlet solution

--- eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
@@ -1,15 +1,27 @@
 package lt.codeacademy.eshop.cart;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping("/cart")
+@SessionAttributes("cartSession")
 public class CartController {
 
   @GetMapping
-  public String openCart() {
+  public String openCart(HttpServletRequest request, Model model) {
+    model.addAttribute("cartSessionValue", (String) request.getSession().getAttribute("cartSession"));
     return "/cart/cart";
+  }
+
+  @GetMapping("/add")
+  public String addToCart(Model model) {
+    model.addAttribute("cartSession", "Labas");
+
+    return "redirect:/cart";
   }
 }
