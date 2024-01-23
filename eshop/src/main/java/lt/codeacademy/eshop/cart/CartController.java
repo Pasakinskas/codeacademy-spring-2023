7f5fd49df840d31ebe5f6f57c3535740f6d2652d
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-23 21:26:42 +0200

    10. [Spring MVC, Session] add empty initial cart page template with controller and translations

---
+++ eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
@@ -0,0 +1,15 @@
+package lt.codeacademy.eshop.cart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+  @GetMapping
+  public String openCart() {
+    return "/cart/cart";
+  }
+}
