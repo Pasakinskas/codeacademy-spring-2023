04c20ab037d64523c966d2a75a6f08c3f06c2cea
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-23 21:45:35 +0200

    10. [Spring MVC, Session] remove Servlet solution and leave spring way retrieve session data

--- eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -13,8 +14,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class CartController {
 
   @GetMapping
-  public String openCart(HttpServletRequest request, Model model) {
-    model.addAttribute("cartSessionValue", (String) request.getSession().getAttribute("cartSession"));
+  public String openCart() {
     return "/cart/cart";
   }
 
