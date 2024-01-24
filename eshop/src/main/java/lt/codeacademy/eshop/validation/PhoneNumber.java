6c312dbf95342b479c21802a0c42db2c9c32cb74
Pasakinskas <marius.pasakinskas@gmail.com>
2024-01-24 21:31:44 +0200

    11. [Custom Validation] add phoneNumber annotation with internal state

--- eshop/src/main/java/lt/codeacademy/eshop/validation/PhoneNumber.java
+++ eshop/src/main/java/lt/codeacademy/eshop/validation/PhoneNumber.java
@@ -17,4 +17,6 @@ public @interface PhoneNumber {
   Class<?>[] groups() default {};
 
   Class<? extends Payload>[] payload() default {};
+
+  PhoneNumberType numberType() default PhoneNumberType.GLOBAL;
 }
--- eshop/src/main/java/lt/codeacademy/eshop/validation/PhoneNumberValidator.java
+++ eshop/src/main/java/lt/codeacademy/eshop/validation/PhoneNumberValidator.java
@@ -5,8 +5,19 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
 
+  private PhoneNumberType type;
+
+  @Override
+  public void initialize(PhoneNumber constraintAnnotation) {
+    this.type = constraintAnnotation.numberType();
+  }
+
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
-    return value.length() == 12;
+    if (type.equals(PhoneNumberType.GLOBAL)) {
+      return value.startsWith("+370") && value.length() == 12;
+    } else {
+      return value.startsWith("86") && value.length() == 9;
+    }
   }
 }
