a6f61df93740b0a37668eb0bf12b14e3718a8b2b
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-29 19:20:34 +0200

    12. [Spring MVC, Session]add product to cart + small UI fixes using display flex

--- eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/cart/CartController.java
@@ -1,31 +1,42 @@
 package lt.codeacademy.eshop.cart;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lt.codeacademy.eshop.product.dto.ProductDto;
+import lt.codeacademy.eshop.product.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cart")
 @SessionAttributes("cartSession")
+@RequiredArgsConstructor
 public class CartController {
+
+  private final ProductService productService;
 
   @ModelAttribute("cartSession")
   public List<ProductDto> createDefaultCartSession() {
-    return List.of(ProductDto.builder()
-      .name("Testas")
-      .build());
+    return new ArrayList<>();
   }
 
   @GetMapping
   public String openCart() {
     return "/cart/cart";
+  }
+
+  @PostMapping("/{productId}")
+  public String addToCart(@PathVariable UUID productId,
+                          @ModelAttribute("cartSession")  List<ProductDto> cart) {
+    final ProductDto productDto = productService.getProductByUUID(productId);
+    cart.add(productDto);
+
+    return "redirect:/products";
   }
 
 }
