6a58783f0edaefa28281d3d002088bb7e65b1f88
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-29 21:19:03 +0200

    12. [Session] calculate cart total price and cart total quantity

--- eshop/src/main/java/lt/codeacademy/eshop/cart/dto/CartDto.java
+++ eshop/src/main/java/lt/codeacademy/eshop/cart/dto/CartDto.java
@@ -1,5 +1,6 @@
 package lt.codeacademy.eshop.cart.dto;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +17,17 @@ public class CartDto {
       .productDto(productDto)
       .quantity(1)
       .build());
+  }
+
+  public BigDecimal getCartTotalPrice() {
+    return cartItems.stream()
+      .map(CartItemDto::getTotalItemPrice)
+      .reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
+
+  public int getCartTotalQuantity() {
+    return cartItems.stream()
+      .map(CartItemDto::getQuantity)
+      .reduce(0, Integer::sum);
   }
 }
