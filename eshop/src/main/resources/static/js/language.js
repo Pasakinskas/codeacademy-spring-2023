4b292e3cb84dd1f9f0084ff0f88eda1547b36839
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-29 18:34:48 +0200

    12. [Spring MVC] fix bootstrap issue, eliminate javascript for language selector, continues web pages design

--- eshop/src/main/resources/static/js/language.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#languageSelector").change(function () {
-        const selectedOption = $("#languageSelector").val();
-        if (selectedOption !== '') {
-            window.location.replace('?lang=' + selectedOption);
-        }
-    });
-})
