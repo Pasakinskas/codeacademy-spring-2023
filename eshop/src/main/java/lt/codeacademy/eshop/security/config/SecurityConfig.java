fb291e80de5ab9b9f99acd4602ee60c9b7b8bdc6
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-30 22:15:59 +0200

    13. [Security Basic] secure add/update/delete page requests and open other for all (without authentication)

--- eshop/src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,7 +17,10 @@ public class SecurityConfig {
     return http
       .authorizeHttpRequests(authorize -> authorize
         .requestMatchers(
-          "/login/**"
+          "/",
+          "/products",
+          "/cart/**",
+          "/users/**"
         ).permitAll()
         .anyRequest()
         .authenticated())
