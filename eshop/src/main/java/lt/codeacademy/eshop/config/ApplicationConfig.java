826ba2d1a3721ad5b4af49f5bbba9e74e1814739
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-30 20:35:46 +0200

    13. [Security Basic] access custom login page using ViewControllerRegistry in WebMvcConfigurer configuration

--- eshop/src/main/java/lt/codeacademy/eshop/config/ApplicationConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/config/ApplicationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -52,6 +53,11 @@ public class ApplicationConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(localeChangeInterceptor());
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/login").setViewName("security/login");
   }
 
 }
