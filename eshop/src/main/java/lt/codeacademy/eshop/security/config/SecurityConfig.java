2748d5915a6d5cb0b33f0a59fa1a7ac62f6860bc
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-01-30 20:54:21 +0200

    13. [Security Basic] fix h2 access

--- eshop/src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,10 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
       .authorizeHttpRequests(authorize -> authorize
-        .requestMatchers("/login/**").permitAll()
+        .requestMatchers(
+          "/login/**",
+          "/eshop_h2/**"
+        ).permitAll()
         .anyRequest()
         .authenticated())
       .formLogin(loginConfigure -> loginConfigure
@@ -24,6 +28,12 @@ public class SecurityConfig {
         .defaultSuccessUrl("/products", true)
         .usernameParameter("loginEmail")    //The HTTP parameter to look for the username
         .passwordParameter("loginPassword") //The HTTP parameter to look for the password
+      )
+      .csrf(csrfConfigurer -> csrfConfigurer
+        .ignoringRequestMatchers("/eshop_h2/**")
+      )
+      .headers(headerConfigurer -> headerConfigurer
+        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
       )
       .build();
   }
