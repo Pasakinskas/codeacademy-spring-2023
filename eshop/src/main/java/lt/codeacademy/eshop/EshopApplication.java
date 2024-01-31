0cdacbcaa15cd151496459cd8d51e2d1de71fd9c
Pasakinskas <marius.pasakinskas@gmail.com>
2024-01-31 20:45:09 +0200

    14. [Profiles] set profile dynamically when starting spring application

--- eshop/src/main/java/lt/codeacademy/eshop/EshopApplication.java
+++ eshop/src/main/java/lt/codeacademy/eshop/EshopApplication.java
@@ -2,11 +2,15 @@ package lt.codeacademy.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class EshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EshopApplication.class, args);
+    SpringApplication app = new SpringApplication(EshopApplication.class);
+    app.setAdditionalProfiles("funny");
+    app.run(args);
 	}
 }
