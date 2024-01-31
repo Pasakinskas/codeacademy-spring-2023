614025f6389986a59080ab5bd0a58aac23d67f6a
Pasakinskas <marius.pasakinskas@gmail.com>
2024-01-31 19:45:28 +0200

    14. [Logging] add logback.xml and disable cors/csrf

--- eshop/src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,6 +33,8 @@ public class SecurityConfig {
         .usernameParameter("loginEmail")    //The HTTP parameter to look for the username
         .passwordParameter("loginPassword") //The HTTP parameter to look for the password
       )
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(AbstractHttpConfigurer::disable)
       .build();
   }
 
--- eshop/src/main/java/lt/codeacademy/eshop/user/controllers/UserController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/user/controllers/UserController.java
@@ -1,6 +1,7 @@
 package lt.codeacademy.eshop.user.controllers;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import lt.codeacademy.eshop.user.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
   @GetMapping("/create")
   public String getUserForm(Model model) {
     model.addAttribute("userDto", UserDto.builder().build());
+    log.info("Got a GET /users/create");
+
     return "user/user";
   }
 
@@ -24,8 +28,7 @@ public class UserController {
     if (errors.hasErrors()) {
       return "user/user";
     }
-    System.out.println("got a successful registration request");
-    System.out.println(userDto);
+    log.info("Got a POST /users/create with {}", userDto);
 
     return "redirect:/users/create";
   }
