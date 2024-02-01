d63890880f41f487ef9ed396e730500a117c11f4
Pasakinskas <marius.pasakinskas@gmail.com>
2024-02-01 21:08:16 +0200

    15. [Testing] add service unit test with mocks and verify calls

--- eshop/src/test/java/lt/codeacademy/eshop/product/service/ProductServiceTest.java
+++ eshop/src/test/java/lt/codeacademy/eshop/product/service/ProductServiceTest.java
@@ -1,23 +1,38 @@
 package lt.codeacademy.eshop.product.service;
 
-import lt.codeacademy.eshop.fakes.FakeProductDao;
 import lt.codeacademy.eshop.product.dao.ProductCategoryRepository;
+import lt.codeacademy.eshop.product.dao.ProductDao;
 import lt.codeacademy.eshop.product.dto.ProductDto;
 import lt.codeacademy.eshop.product.mappers.ProductMapper;
+import lt.codeacademy.eshop.product.pojo.Product;
 import lt.codeacademy.eshop.product.pojo.ProductCategory;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
-
-
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
+
+  @Mock
+  private ProductCategoryRepository productCategoryRepository;
+
+  @Mock
+  private ProductDao productDao;
+
+  @Mock
+  private ProductMapper mapper;
+
+  @InjectMocks
+  private ProductService service;
 
   @Test
   public void saveProduct_savesAProductAndSetsCategory() {
@@ -25,26 +40,15 @@ public class ProductServiceTest {
     var categoryId = 123L;
 
     var productDto = new ProductDto(productUUID, "strawberry", BigDecimal.valueOf(1.95), 1, categoryId);
-    var productCategory = new ProductCategory(categoryId, "food");
+    var product = new Product();
 
-    var productCategoryRepository = mock(ProductCategoryRepository.class);
-    when(productCategoryRepository.getReferenceById(categoryId)).thenReturn(productCategory);
+    when(productCategoryRepository.getReferenceById(any())).thenReturn(new ProductCategory());
+    when(mapper.fromDto(productDto)).thenReturn(product);
 
-    var fakeProductDao = new FakeProductDao();
+    service.saveProduct(productDto);
 
-    var productsService = new ProductService(fakeProductDao, productCategoryRepository, new ProductMapper());
-
-    productsService.saveProduct(productDto);
-
-    var savedProduct = fakeProductDao.getProductByUUID(productUUID)
-      .orElseThrow(() -> new RuntimeException("Product was not saved"));
-
-    var categoryOnProduct = savedProduct.getProductCategories().stream().findFirst()
-      .orElseThrow(() -> new RuntimeException("Product has no category"));
-
-    assertEquals(categoryOnProduct.getId(), categoryId);
-    assertEquals(categoryOnProduct.getName(), productCategory.getName());
-
-    assertTrue(savedProduct.getId() > 0);
+    verify(mapper).fromDto(eq(productDto));
+    verify(productCategoryRepository).getReferenceById(eq(categoryId));
+    verify(productDao).save(eq(product));
   }
 }
