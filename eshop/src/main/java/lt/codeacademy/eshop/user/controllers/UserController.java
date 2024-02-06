e1cec75d77e88a52953b69d9f609874bf5c75a72
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-02-06 18:27:05 +0200

    18. [Security Basic] have full working user registration form with PasswordEncoder bean (!important, have to separated in different services)

--- eshop/src/main/java/lt/codeacademy/eshop/user/controllers/UserController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/user/controllers/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lt.codeacademy.eshop.user.dto.UserDto;
+import lt.codeacademy.eshop.user.service.UsersRegistrationService;
 import lt.codeacademy.eshop.user.service.UsersService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
   private final UsersService usersService;
+  private final UsersRegistrationService usersRegistrationService;
 
   @GetMapping("/create")
   public String getUserForm(Model model) {
@@ -33,7 +35,7 @@ public class UserController {
     if (errors.hasErrors()) {
       return "user/user";
     }
-    usersService.register(userDto);
+    usersRegistrationService.register(userDto);
 
     return "redirect:/users/create";
   }
--- eshop/src/main/java/lt/codeacademy/eshop/user/service/UsersRegistrationService.java
+++ eshop/src/main/java/lt/codeacademy/eshop/user/service/UsersRegistrationService.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lt.codeacademy.eshop.user.dto.UserDto;
 import lt.codeacademy.eshop.user.pojo.User;
 import lt.codeacademy.eshop.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,13 +14,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UsersService implements UserDetailsService {
+public class UsersRegistrationService {
 
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return userRepository.findUserByEmail(username)
-      .orElseThrow(() -> new UsernameNotFoundException("'" + username + "' not found!"));
+  public void register(UserDto userDto) {
+    userRepository.save(
+      User.builder()
+        .name(userDto.getName())
+        .surname(userDto.getSurname())
+        .email(userDto.getEmail())
+        .phoneNumber(userDto.getPhoneNumber())
+        .zipCode(userDto.getZipCode())
+        .password(new BCryptPasswordEncoder().encode(userDto.getPassword()))
+        .build()
+    );
   }
 }
