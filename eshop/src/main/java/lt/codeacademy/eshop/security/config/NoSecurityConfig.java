9b2a8c8d44ba8d6897bdff3ac13c5a7b1e971b97
Pasakinskas <marius.pasakinskas@gmail.com>
2024-02-08 19:55:51 +0200

    20. [RESTful APIs] disable spring security using profiles

--- eshop/src/main/java/lt/codeacademy/eshop/security/config/NoSecurityConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/config/NoSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,5 +33,10 @@ public class NoSecurityConfig {
       .requestMatchers(
         PathRequest.toStaticResources().atCommonLocations()
       );
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
