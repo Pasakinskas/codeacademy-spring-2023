1c9174511e6a102fba6c8d9fdd2f3cb1c03edf82
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-02-13 21:42:27 +0200

    22. [RESTful APIs, Security Basic] configure basic security and use stateless session policy, off csrf and turn off standard spring security basic form view

--- eshop/security/src/main/java/lt/codeacademy/eshop/security/config/basic/SecurityRestBasicConfig.java
+++ eshop/security/src/main/java/lt/codeacademy/eshop/security/config/basic/SecurityRestBasicConfig.java
@@ -2,18 +2,23 @@ package lt.codeacademy.eshop.security.config.basic;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lt.codeacademy.eshop.security.config.CommonConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
@@ -24,9 +29,28 @@ import java.util.List;
 @EnableConfigurationProperties(ApplicationUsersPropertyConfig.class)
 @RequiredArgsConstructor
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityRestBasicConfig {
+public class SecurityRestBasicConfig extends CommonConfig  {
 
   private final ApplicationUsersPropertyConfig applicationUsers;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+      // Disable csrf as not important for rest endpoints
+      .csrf(AbstractHttpConfigurer::disable)
+
+      // turn on httpBasic of InMemoryUserDetailsManager
+      .httpBasic(httpBasicConfigurer -> httpBasicConfigurer.init(http))
+
+      // set session management to stateless
+      .sessionManagement(sessionManagementConfigurer ->
+        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+      // set authorization request access for whole requests
+      .authorizeHttpRequests(authConfigurer -> authConfigurer.anyRequest().authenticated())
+
+      .build();
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
