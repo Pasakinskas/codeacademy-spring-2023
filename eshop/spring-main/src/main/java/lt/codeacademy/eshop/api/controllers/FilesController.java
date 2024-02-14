d6fb50f6d918e3bd36abd0b4a8cbef18684f4598
Pasakinskas <marius.pasakinskas@gmail.com>
2024-02-14 19:40:08 +0200

    23. [RESTful APIs] add controller method to get file contents

--- eshop/spring-main/src/main/java/lt/codeacademy/eshop/api/controllers/FilesController.java
+++ eshop/spring-main/src/main/java/lt/codeacademy/eshop/api/controllers/FilesController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 public class FilesController {
 
@@ -22,5 +24,10 @@ public class FilesController {
   public ResponseEntity<Void> uploadAFile(@RequestBody MultipartFile file) {
     fileService.save(file);
     return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("files/contents")
+  public ResponseEntity<List<String>> getFileContents(@RequestBody MultipartFile file) {
+    return ResponseEntity.ok().body(fileService.getTextFileContents(file));
   }
 }
--- eshop/spring-main/src/main/java/lt/codeacademy/eshop/common/file/FileService.java
+++ eshop/spring-main/src/main/java/lt/codeacademy/eshop/common/file/FileService.java
@@ -1,12 +1,16 @@
 package lt.codeacademy.eshop.common.file;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * read info from a file
@@ -19,10 +23,24 @@ public class FileService {
 
   public void save(MultipartFile file) {
     try {
-      new File("hello-world");
-      file.transferTo(new File(fileLocation + "/" + file.getOriginalFilename()));
+      var newFile = new File(generateFilename(file));
+      file.transferTo(newFile);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public List<String> getTextFileContents(MultipartFile file) {
+    try {
+      var reader = new InputStreamReader(file.getInputStream());
+      var bufferedReader = new BufferedReader(reader);
+      return bufferedReader.lines().toList();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String generateFilename(MultipartFile file) {
+    return fileLocation + "/" + file.getOriginalFilename();
   }
 }
