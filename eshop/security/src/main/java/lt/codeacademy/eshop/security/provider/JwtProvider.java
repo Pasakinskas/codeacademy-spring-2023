0c4670514e491fc5663228e5aeb8862e3ae0919a
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-02-22 19:00:20 +0200

    27. [RESTful APIs, Security JWT] create JWT token when login using secret key and expiration time (authentication)

--- eshop/security/src/main/java/lt/codeacademy/eshop/security/provider/JwtProvider.java
+++ eshop/security/src/main/java/lt/codeacademy/eshop/security/provider/JwtProvider.java
@@ -1,15 +1,52 @@
 package lt.codeacademy.eshop.security.provider;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lt.codeacademy.eshop.security.dto.UserPrincipalDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
 
+  private final static Date NOW = new Date();
+
+  @Value("#{${spring.security.jwt.validity-time} * 60 * 1000}")
+  private long tokenValidityInMillis;
+
+  @Value("${spring.security.jwt.secret-key}")
+  private byte[] secretKey;
+
+  public static Map<String, Object> headerMap;
+
+  static {
+    headerMap = new HashMap<>();
+    headerMap.put("typ", "JWT");
+  }
+
   public String getToken(Authentication authentication) {
     final UserPrincipalDto principalDto = (UserPrincipalDto) authentication.getPrincipal();
 
-    return "cia mano tokenas";
+    final String token = JWT.create()
+      .withIssuer("eshop-api")
+      .withAudience("eshop-api")
+      .withExpiresAt(new Date(NOW.getTime() + tokenValidityInMillis))
+      .withIssuedAt(NOW)
+      .withHeader(headerMap)
+      .withSubject(principalDto.getUsername())
+      .withClaim("roles", principalDto.getAuthorities().stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.toList()))
+      .sign(Algorithm.HMAC512(secretKey));
+
+    return token;
   }
+
 }
