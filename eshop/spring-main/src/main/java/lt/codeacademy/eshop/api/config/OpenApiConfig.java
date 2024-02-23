380d02f5a16aab2f43d5d617867326836d89d691
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2024-02-23 19:39:09 +0200

    28. [RESTful APIs, Swagger, OpenAPI] add jwt security scheme into documentation

--- eshop/spring-main/src/main/java/lt/codeacademy/eshop/api/config/OpenApiConfig.java
+++ eshop/spring-main/src/main/java/lt/codeacademy/eshop/api/config/OpenApiConfig.java
@@ -1,11 +1,14 @@
 package lt.codeacademy.eshop.api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,7 +16,8 @@ public class OpenApiConfig {
   @Bean
   public OpenAPI customOpenApi() {
     return new OpenAPI()
-      .info(getInfo());
+      .info(getInfo())
+      .components(getComponents());
   }
 
   private Info getInfo() {
@@ -23,5 +27,17 @@ public class OpenApiConfig {
       .description("This is documentation using swagger and openApi")
       .contact(new Contact().url("www.eshop.lt").name("Eshop UAB"))
       .license(new License().name("No licence. Free to use"));
+  }
+
+  private Components getComponents() {
+    SecurityScheme securitySchemeBearer = new SecurityScheme()
+      .name(HttpHeaders.AUTHORIZATION)
+      .description("Bearer authorization. To get required key you should call /login")
+      .type(SecurityScheme.Type.HTTP)
+      .scheme("bearer")
+      .bearerFormat("JWT");
+
+    return new Components()
+      .addSecuritySchemes(securitySchemeBearer.getName(), securitySchemeBearer);
   }
 }
